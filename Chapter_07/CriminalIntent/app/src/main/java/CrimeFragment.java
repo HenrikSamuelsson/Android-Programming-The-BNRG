a2c4346530076d0ea6664e7c9bb43a6a335e69cd
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2016-06-26 17:52:26 +0200

    Listing 7.9: This class shall extend fragment.

--- Chapter_07/CriminalIntent/app/src/main/java/CrimeFragment.java
+++ Chapter_07/CriminalIntent/app/src/main/java/CrimeFragment.java
@@ -1,5 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
 /**
  * Created by Henrik Samuelsson on 2016-06-26.
  */
-public class CrimeFragment {
+public class CrimeFragment extends Fragment{
 }
