e41708be87790344a1223f5e493cca4e2cefd45c
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2016-06-26 22:00:15 +0200

    Listing 8.2: Generated getters and setters.

--- Chapter_08/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ Chapter_08/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -10,6 +10,8 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+
+
     private Date mDate;
     private boolean mSolved;
 
@@ -30,5 +32,21 @@ public class Crime {
 
     public String getTitle() {
         return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
