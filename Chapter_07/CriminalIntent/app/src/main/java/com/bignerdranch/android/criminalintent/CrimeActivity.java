b1dbb96df71952426d02eb4f1f22f35b4c556090
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2016-06-26 20:17:11 +0200

    Adding the fragment to the activity so that it is displayed.
    This concludes chapter 7.

--- Chapter_07/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ Chapter_07/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
@@ -12,5 +13,11 @@ public class CrimeActivity extends FragmentActivity {
         setContentView(R.layout.activity_crime);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
