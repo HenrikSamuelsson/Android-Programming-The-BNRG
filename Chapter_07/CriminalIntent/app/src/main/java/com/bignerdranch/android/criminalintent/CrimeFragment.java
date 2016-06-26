b62bb3789329dfbec69a2fba2dec5c11664eb67b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2016-06-26 18:22:55 +0200

    Listing 7.12: Inflating the fragment.

--- Chapter_07/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ Chapter_07/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -2,6 +2,9 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Henrik Samuelsson on 2016-06-26.
@@ -13,5 +16,11 @@ public class CrimeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    @Override
+    public void onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = layoutInflater.inflate(R.layout.fragment_crime, container, false);
+        return view;
     }
 }
